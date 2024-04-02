fb2029441e272024857f599e00beeae0aa5bb044
kyleglowacki <kyleglowacki@yahoo.com>
2024-04-02 08:07:56 -0400

    Update obvious.py

--- obvious.py
+++ obvious.py
@@ -1,4 +1,4 @@
-import sudoku_utils
+import sudoku_utils as su
 
 def sudoku_rule_only_choice(grid):
     updates = False
@@ -7,11 +7,11 @@ def sudoku_rule_only_choice(grid):
         for col in range(9):
             if len(grid[row][col].get_possible_digits()) == 1:
                 # if so.. mark blank with missing number.
-                grid = set_digit_on_grid(grid, row, col, grid[row][col].get_possible_digits()[0])
+                digit = list(grid[row][col].get_possible_digits())[0]
+                grid = su.set_digit_on_grid(grid, row, col, digit)
                 updates = True
     return updates, grid
 
 # only one blank in row can be a particular digit
 # only one blank in col can be a particular digit
 # only one blank in box can be a particular digit
-
